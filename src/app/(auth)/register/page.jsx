e973d3bc9390752859fd802c1fbfc5d3ca4c93db
bzzoo <fullbster@gmail.com>
2024-12-07 22:41:46 +0900

    [init] 의존성 설정, 기본 페이지 정의

---
+++ src/app/(auth)/register/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Resiter = () => {
+  return <div>Resiter</div>;
+};
+
+export default Resiter;
---
+++ src/app/(order)/order-details/[id]/page.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const OrderDetails = ({ params, searchParams }) => {
+  const { id } = params;
+  const { query } = searchParams;
+  return <div>OrderDetails</div>;
+};
+
+export default OrderDetails;
